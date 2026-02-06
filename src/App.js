d0d0a4af9cf3c485e6a20d19dd0006b1780ec8ce
sgn08062 <sgn08062@gmail.com>
2026-02-06 16:24:56 +0900

    Create AuthHandler

--- src/App.js
+++ src/App.js
@@ -11,22 +11,31 @@ import MyPage from "./component/myPage/MyPage";
 import ModProfile from "./component/modProfile/ModProfile";
 import MainLayout from "./layout/MainLayout";
 import LandingPage from "./component/LandingPage/LandingPage";
+import AuthHandler from "./auth/AuthHandler"; // [추가] 경로가 맞는지 확인해주세요!
 
 function App() {
 
   return(
     <Routes>
+      {/* 1. 공개 라우트: 로그인 없이 접속 가능 */}
         <Route path={'/'} element={<LandingPage />} />
         <Route path={'/login'} element={<Login/>}/>
         <Route path={'/signup'} element={<Signup/>}/>
-      <Route element={<MainLayout />}>
-        <Route path={'/myPage'} element={<MyPage/>}/>
-        <Route path={'/modProfile'} element={<ModProfile/>}/>
-        <Route path={'/projectList'} element={<ProjectList/>}/>
-        <Route path={'/projectDetail'} element={<ProjectDashBoard/>}/>
-        <Route path={'/project/:projectId/dashboard'} element={<ProjectDashBoard/>}/>
-        <Route path={'/aiReport'} element={<AiReport/>}/>
-        <Route path={'/final-report/create'} element={<FinalReportCreatePage/>}/>
+
+      {/* 2. 보호된 라우트: 로그인이 있어야만 접근 가능 */}
+      {/* 가장 바깥에서 AuthHandler가 토큰을 검사합니다. */}
+      <Route element={<AuthHandler />}>
+          {/* 검사가 통과되면 MainLayout(상단바, 사이드바 등)이 그려집니다. */}
+          <Route element={<MainLayout />}>
+            {/* 그 안에 실제 페이지 내용이 들어갑니다. */}
+            <Route path={'/myPage'} element={<MyPage/>}/>
+            <Route path={'/modProfile'} element={<ModProfile/>}/>
+            <Route path={'/projectList'} element={<ProjectList/>}/>
+            <Route path={'/projectDetail'} element={<ProjectDashBoard/>}/>
+            <Route path={'/project/:projectId/dashboard'} element={<ProjectDashBoard/>}/>
+            <Route path={'/aiReport'} element={<AiReport/>}/>
+            <Route path={'/final-report/create'} element={<FinalReportCreatePage/>}/>
+          </Route>
       </Route>
     </Routes>
   );
